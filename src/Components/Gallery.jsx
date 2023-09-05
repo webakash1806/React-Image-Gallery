90be8f31431159eb41dbc53addaa12260335fc6d
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-09-05 13:48:55 +0530

    Home Styles and data fetched

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,8 +1,8 @@
 const Gallery = ({ image }) => {
     return (
-        <div>
-            <img src={image} alt="" />
-        </div>
+        <>
+            <img src={image} alt="" className="sm:hover:scale-[1.1] duration-[0.3s] w-[18.5rem] h-[17rem] object-cover rounded-[1rem] hover:cursor-pointer shadow-md shadow-slate-500 hover:shadow-xl hover:shadow-slate-700" />
+        </>
     )
 }
 
--- src/Components/GalleryData.jsx
+++ src/Components/GalleryData.jsx
@@ -9,7 +9,7 @@ const GalleryData = () => {
 
     const imageData = async () => {
         setIsLoading(true)
-        const response = await axios.get("https://api.slingacademy.com/v1/sample-data/photos")
+        const response = await axios.get("https://api.slingacademy.com/v1/sample-data/photos?offset=0&limit=20")
         const mainData = response.data.photos
         const originalData = mainData.map((d) => {
             return {
@@ -23,14 +23,17 @@ const GalleryData = () => {
 
     console.log(dataFetched)
 
-    useEffect(() => { imageData() }, [setDataFetched])
+    useEffect(() => { imageData() }, [])
 
     return (
-        <>
-            <p>Image Gallery</p>
-            {isLoading ? <div className='w-[3.5rem] h-[3.5rem] border-[6.2px] rounded-full border-r-blue-500 animate-spin'></div> :
-                dataFetched.map((d) => <Gallery key={d.id} image={d.image} />)}
-        </>
+        <div className='w-screen flex flex-col justify-center items-center gap-4'>
+            <p className='font-bold font-serif text-4xl py-4 w-full text-center tracking-wide'>Image Gallery</p>
+
+            <div className="image-container flex flex-wrap items-center justify-center gap-7">
+                {isLoading ? <div className='w-[3.7rem] h-[3.7rem] border-[6.2px] rounded-full border-r-blue-500 animate-spin mt-[7rem]'></div> :
+                    dataFetched.map((d) => <Gallery key={d.id} image={d.image} />)}
+            </div>
+        </div>
     )
 }
 
